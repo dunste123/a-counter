5855288d94853d810aa28c7beb668b79e5459b31
Duncan Sterken <duncan.sterken123@gmail.com>
2019-10-01 10:26:05 +0200

    Add a button

--- index.py
+++ index.py
@@ -1,19 +1,16 @@
 import pygame
 import youtube
-import threading
+import webbrowser
 from StoppableThread import StoppableThread
 import sched
 import time
-import sys
 from pygame.locals import *
+from colors import *
+from button import Button
 
 pygame.init()
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-grey = (112, 128, 144)
 
 screen_w = screen.get_width()
 screen_h = screen.get_height()
@@ -39,37 +36,41 @@ channel_text_y = screen_h * .1
 channel_name_text_x = screen_w * .5
 channel_name_text_y = screen_h * .1
 
-headerFont = pygame.font.SysFont("Arial", 50)
-digitFont = pygame.font.SysFont("Arial", 50)
-
+headerFont = pygame.font.SysFont('Arial', 50)
+digitFont = pygame.font.SysFont('Arial', 50)
 subs = 0
 views = 0
 videos = 0
 channelName = 'null'
+latest_video_id = youtube.default_video_id
+button_text = 'View latest video'
+button_width = len(button_text) * 25
+button = Button(green, (screen_w - button_width) / 2, screen_h * .75, button_width, 100, button_text)
 
 
 def draw_hud():
     screen.fill(grey)
-
     pygame.draw.circle(screen, black, (int(circle1_x), int(circle_y)), int(circle_rad), 5)
     pygame.draw.circle(screen, black, (int(circle2_x), int(circle_y)), int(circle_rad), 5)
     pygame.draw.circle(screen, black, (int(circle3_x), int(circle_y)), int(circle_rad), 5)
 
-    channel_text = headerFont.render("Channel: ", True, black)
+    channel_text = headerFont.render('Channel: ', True, black)
     channel_text_loc = channel_text.get_rect(center=(channel_text_x, channel_text_y))
     screen.blit(channel_text, channel_text_loc)
 
-    subs_text = headerFont.render("Subs", True, black)
+    subs_text = headerFont.render('Subs', True, black)
     subs_text_loc = subs_text.get_rect(center=(subs_text_x, subs_text_y))
     screen.blit(subs_text, subs_text_loc)
 
-    views_text = headerFont.render("Views", True, black)
+    views_text = headerFont.render('Views', True, black)
     views_text_loc = views_text.get_rect(center=(views_text_x, views_text_y))
     screen.blit(views_text, views_text_loc)
 
-    videos_text = headerFont.render("Videos", True, black)
+    videos_text = headerFont.render('Videos', True, black)
     videos_text_loc = videos_text.get_rect(center=(videos_text_x, videos_text_y))
     screen.blit(videos_text, videos_text_loc)
+
+    button.draw(screen, black)
 
 
 running = True
@@ -77,13 +78,13 @@ s = sched.scheduler(time.time, time.sleep)
 
 
 def update_counts(sc):
-    global subs, views, videos, channelName
+    global subs, views, videos, channelName, latest_video_id
     data = youtube.get_subs_and_views()
     subs = data['subs']
     views = data['views']
     videos = data['videos']
     channelName = data['name']
-
+    latest_video_id = youtube.get_latest_video_id()
     # every 60 seconds
     s.enter(60, 1, update_counts, (sc,))
 
@@ -92,6 +93,16 @@ def counter_thread():
     global s
     s.enter(1, 1, update_counts, (s,))
     s.run()
+
+
+def open_video_in_browser():
+    webbrowser.open('https://www.youtube.com/watch?v=' + latest_video_id)
+    call_quit_event()
+
+
+def call_quit_event():
+    event = pygame.event.Event(QUIT)
+    pygame.event.post(event)
 
 
 counter_thread = StoppableThread(target=counter_thread, daemon=True)
@@ -103,30 +114,38 @@ def run_display():
 
     while running:
         for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE or event.type == QUIT:
-                    running = False
-                    run_thread.stop()
-                    counter_thread.stop()
-                    # sys.exit()
+            pos = pygame.mouse.get_pos()
+            if event.type == MOUSEBUTTONDOWN:
+                if button.isOver(pos):
+                    open_video_in_browser()
+            elif event.type == MOUSEMOTION:
+                if button.isOver(pos):
+                    button.color = dark_green
+                else:
+                    button.color = green
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    call_quit_event()
+            elif event.type == QUIT:
+                running = False
+                run_thread.stop()
+                counter_thread.stop()
+                quit(0)
+
         draw_hud()
 
-        subsDisplay = digitFont.render(str(subs), 3, white)
-        viewsDisplay = digitFont.render(str(views), 3, white)
-        videosDisplay = digitFont.render(str(videos), 3, white)
-
-        subWith, subHeight = digitFont.size(str(subs))
-        viewWith, viewHeight = digitFont.size(str(views))
-        videoWith, videoHeight = digitFont.size(str(videos))
-
-        screen.blit(subsDisplay, (circle1_x - (subWith / 2), circle_y - 32))
-        screen.blit(viewsDisplay, (circle2_x - (viewWith / 2), circle_y - 32))
-        screen.blit(videosDisplay, (circle3_x - (videoWith / 2), circle_y - 32))
-
+        subs_display = digitFont.render(str(subs), 3, white)
+        views_display = digitFont.render(str(views), 3, white)
+        videos_display = digitFont.render(str(videos), 3, white)
+        sub_with, sub_height = digitFont.size(str(subs))
+        view_with, view_height = digitFont.size(str(views))
+        video_with, video_height = digitFont.size(str(videos))
+        screen.blit(subs_display, (circle1_x - (sub_with / 2), circle_y - 32))
+        screen.blit(views_display, (circle2_x - (view_with / 2), circle_y - 32))
+        screen.blit(videos_display, (circle3_x - (video_with / 2), circle_y - 32))
         channel_name_text = headerFont.render(channelName, True, white)
         channel_name_text_loc = channel_name_text.get_rect(center=(channel_name_text_x, channel_name_text_y))
         screen.blit(channel_name_text, channel_name_text_loc)
-
         pygame.display.update()
         pygame.display.flip()
 
--- youtube.py
+++ youtube.py
@@ -1,40 +1,66 @@
 from config import config
 import requests
 
-base_url = "https://www.googleapis.com/youtube/v3"
+base_url = 'https://www.googleapis.com/youtube/v3'
 key = config['api_key']
 channel = config['channel_id']
 empty_data = {
-    'name': 'null',
+    'name': 'Orange',
     'subs': 194,
     'views': 4875,
     'videos': 90
 }
+default_video_id = 'dQw4w9WgXcQ'
 
 
 def get_subs_and_views():
-    global base_url, key, channel, empty_data
-
     return empty_data
 
-    params = {
-        'part': 'id,statistics,snippet',
-        'key': key,
-        'id': channel
-    }
+    try:
+        params = {
+            'part': 'id,statistics,snippet',
+            'key': key,
+            'id': channel
+        }
 
-    r = requests.get(url=f"{base_url}/channels", params=params)
-    data = r.json()
+        r = requests.get(url=base_url + '/channels', params=params)
+        data = r.json()
 
-    if data['pageInfo']['totalResults'] == 0:
+        if data['pageInfo']['totalResults'] == 0:
+            return empty_data
+
+        stats = data['items'][0]['statistics']
+        title = data['items'][0]['snippet']['title']
+
+        return {
+            'name': title,
+            'subs': stats['subscriberCount'],
+            'views': stats['viewCount'],
+            'videos': stats['videoCount'],
+        }
+    except:
         return empty_data
 
-    stats = data['items'][0]['statistics']
-    title = data['items'][0]['snippet']['title']
 
-    return {
-        'name': title,
-        'subs': stats['subscriberCount'],
-        'views': stats['viewCount'],
-        'videos': stats['videoCount'],
-    }
+def get_latest_video_id():
+    return default_video_id
+
+    try:
+        params = {
+            'part': 'id,snippet',
+            'key': key,
+            'channelId': channel,
+            'order': 'date'
+        }
+
+        r = requests.get(url=base_url + '/search', params=params)
+        data = r.json()
+
+        if data['pageInfo']['totalResults'] == 0:
+            return empty_data
+
+        for item in data['items']:
+            if item['id']['kind'] == 'youtube#video':
+                return item['id']['videoId']
+    except:
+        return default_video_id
